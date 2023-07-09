54a0ffdddfd8592e0681a4c4a05c82a9cb58f43f
1amSumit <sj779619@gmail.com>
2023-07-09 11:10:19 +0530

    toggle menus implemeted

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -1,6 +1,10 @@
+import { createContext, useContext, useState } from "react";
+import { createPortal } from "react-dom";
+import { HiEllipsisVertical } from "react-icons/hi2";
 import styled from "styled-components";
+import useClick from "../hooks/useClick";
 
-const StyledMenu = styled.div`
+const Menu = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
@@ -60,3 +64,78 @@ const StyledButton = styled.button`
     transition: all 0.3s;
   }
 `;
+
+const MenusCtx = createContext();
+
+const Menus = ({ children }) => {
+  const [openId, setOpenId] = useState("");
+  const [position, setPosition] = useState(null);
+
+  const close = () => setOpenId("");
+
+  const open = setOpenId;
+
+  return (
+    <MenusCtx.Provider value={{ openId, open, close, position, setPosition }}>
+      {children}
+    </MenusCtx.Provider>
+  );
+};
+
+const Toggle = ({ id }) => {
+  const { openId, open, close, setPosition } = useContext(MenusCtx);
+
+  const handleClick = (e) => {
+    const rect = e.target.closest("button").getBoundingClientRect();
+
+    setPosition({
+      x: window.innerWidth - rect.width - rect.x,
+      y: rect.y + rect.height + 8,
+    });
+    openId === "" || openId !== id ? open(id) : close();
+  };
+
+  return (
+    <StyledToggle onClick={handleClick}>
+      <HiEllipsisVertical />
+    </StyledToggle>
+  );
+};
+
+const List = ({ id, children }) => {
+  const { openId, position, close } = useContext(MenusCtx);
+
+  const ref = useClick(close);
+
+  if (id !== openId) return null;
+
+  return createPortal(
+    <StyledList position={position} ref={ref}>
+      {children}
+    </StyledList>,
+    document.body
+  );
+};
+
+const Button = ({ children, icon, onClick }) => {
+  const { close } = useContext(MenusCtx);
+  const handleClick = () => {
+    onClick?.();
+    close();
+  };
+  return (
+    <li>
+      <StyledButton onClick={handleClick}>
+        {icon}
+        <span>{children}</span>
+      </StyledButton>
+    </li>
+  );
+};
+
+Menus.Menu = Menu;
+Menus.Toggle = Toggle;
+Menus.List = List;
+Menus.Button = Button;
+
+export default Menus;
--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -90,7 +90,10 @@ const Row = ({ children }) => {
   );
 };
 
-const Body = ({ children }) => {};
+const Body = ({ data, render }) => {
+  if (!data.length) return <Empty>No Data to show at the moment!</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
+};
 
 Table.Header = Header;
 Table.Row = Row;
