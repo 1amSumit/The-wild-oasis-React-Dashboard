12b24fd37249a48a5b48ccd44e22350c84e24e0b
1amSumit <sj779619@gmail.com>
2023-07-15 09:18:54 +0530

    Sort implemented in CLient side

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -50,6 +50,7 @@ const Filter = ({ filterField, options }) => {
           key={option.value}
           onClick={() => handleClick(option.value)}
           active={option.value === currentFilteredValue}
+          disabled={option.value === currentFilteredValue}
         >
           {option.label}
         </FilterButton>
